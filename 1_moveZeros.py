55acd8560c18b16cc2e0504e5a8a1c9a4ce55420
Rohit Pandhare <93035996+rohitpandhare@users.noreply.github.com>
2024-12-20 22:05:33 +0530

    Update 1_moveZeros.py

--- 1_moveZeros.py
+++ 1_moveZeros.py
@@ -6,10 +6,12 @@ class Solution(object):
           #when the elements in array are non-zero
             if nums[i] != 0:
               # copy the current element to the pos 
+                # the current VALUE is updated the POS position
                 nums[pos] = nums[i]
               #update the pos -- so that will be used in next for loop for iteration and zero addition
                 pos +=1
 
+        # start from end of non-zero element (POS) to end of nums array
         for i in range(pos,len(nums)):
           #only append required zeros
             nums[i] = 0

748ac8961d8954e140acedf235c77f22616a6e71
Rohit Pandhare <93035996+rohitpandhare@users.noreply.github.com>
2024-12-21 21:34:39 +0530

    Update 2_mergeSortedArray.py

--- 2_mergeSortedArray.py
+++ 2_mergeSortedArray.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
+        #start for loop with length of second array-- 
         for i in range(n):
+            #and then insert ---m-- + with nums2 elements
             nums1[m+i] = nums2[i]
+        #simple sort 
         nums1.sort()
 

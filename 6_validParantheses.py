00962818afe0dda0a3dede87a7bd8f60f56afca6
Rohit Pandhare <93035996+rohitpandhare@users.noreply.github.com>
2024-12-25 23:26:11 +0530

    Create 6_validParantheses.py

---
+++ 6_validParantheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        # Mappping the closing bracket with opning
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        # Initializing an empty stack 
+        stack = []
+        
+        # Iterate over each element
+        for element in s:
+            # Check if the character is a closing bracket
+            if element in bracket_map:
+                # Pop the top element from the stack if it's not empty--- else use '#' as a placeholder
+                top_element = stack.pop() if stack else '#' #storing the top of stack
+                
+                # Check if the popped bracket with char map's value part ( key - end brack & val - opening part)
+                if bracket_map[char] != top_element:
+                    return False  # Return False if mismatch
+            else:
+                # If it's an opening bracket then push into stack
+                stack.append(char)
+        
+        # at the end the stack will become empty
+        return not stack  # Return True if the stack is empty -- stack -empty- false--> not stack -- true !!

6e4eb1daabd5dc9e74a83daec20e448da99ac4b0
Rohit Pandhare <93035996+rohitpandhare@users.noreply.github.com>
2024-12-28 22:34:08 +0530

    Create 9_findFirstOccuranceOfString.py

---
+++ 9_findFirstOccuranceOfString.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        # Handle the case of an empty needle
+        if not needle:
+            return 0
+        
+        # Get the length of needle to use for slicing
+        e = len(needle)
+        
+        # Loop through the haystack
+        for i in range(len(haystack) - e + 1):
+            # Check if the current slice matches the needle (from i-th element to i + len(needle) )
+            if haystack[i:i+e] == needle:
+                return i
+        
+        # else
+        return -1
